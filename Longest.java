f0053e4f7a7c8bfeffba932fbb37caeba9fb65e2
kavinkumar06 <kavinm09@gmail.com>
2017-08-07 17:49:30 +0530

    Create Longest.java

---
+++ Longest.java
@@ -0,0 +1,24 @@
+# Longest
+import java.io.*;
+import java.util.*;
+
+public class Longest
+{
+		public static void main(String[] args) 
+		{
+			Scanner s = new Scanner(System.in);
+			System.out.println("Enter the string:");
+			String s1 = s.next();
+			String sr = "";
+			for (int i = 0; i < s1.length() - 1; i++)
+			{
+				if (s1.charAt(i) < s1.charAt(i + 1)) 
+				{
+					sr += s1.charAt(i);
+				}
+			}
+      System.out.println("The Lexo string is:");
+			System.out.println(sr + s1.charAt(s1.length() - 1));
+			}
+
+	}
